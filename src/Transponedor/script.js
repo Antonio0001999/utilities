24232feed9026b06f4df23e711b50927290d05df
Antonio <99355000+Antonio0001999@users.noreply.github.com>
2023-09-17 11:19:10 +0200

    11:19 - Delete Files
    Deleted useless files.

--- src/Transponedor/script.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', main);
-
-function main() {
-	let btn = document.querySelector('#btnTransponer');
-	btn.addEventListener('click', () => {
-		transponer('tabla');
-	});
-}
